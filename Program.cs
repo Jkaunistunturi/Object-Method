ab883e92307bb7a59a781c870ed10f88d214232b
JK <jenny.kaunistunturi@edunord.se>
2024-06-14 07:55:57 +0200

    Added getters and setters

--- Program.cs
+++ Program.cs
@@ -6,15 +6,17 @@
         {
 
             //Here we define students with constructor
-            Student student1 = new Student("K", "Programming", 2.8);
-            Student student2 = new Student("L", "Art", 3.6);
-            Student student3 = new Student("A", "Philosophy", 3.1);
+            Student student1 = new Student("Ken", "Programming", 2.8, "A");
+            Student student2 = new Student("Lisa", "Art", 3.6, "B");
+            Student student3 = new Student("Ted", "Philosophy", 3.1, "A");
+            Student student4 = new Student("Ann", "Psychology", 3.5, "P");
 
 
             //Here we check if each student has honors, calling the object method
-            Console.WriteLine(student1.HasHonors());
-            Console.WriteLine(student2.HasHonors());
-            Console.WriteLine(student3.HasHonors());
+            Console.WriteLine(student1.Letter);
+            Console.WriteLine(student2.Letter);
+            Console.WriteLine(student3.Letter);
+            Console.WriteLine(student4.Letter); //this student does not have any of the defined letter, so the letter is set to "No letter"
 
             Console.ReadLine();
         }
--- Student.cs
+++ Student.cs
@@ -11,14 +11,43 @@ namespace Object_Method
         public string name;
         public string major;
         public double gpa;
+        private string letter;
+
 
         //Constructor
-        public Student(string aName, string aMajor, double aGpa)
+        public Student(string aName, string aMajor, double aGpa, string aLetter)
         {
             name = aName;
             major = aMajor;
             gpa = aGpa;
+           // letter = aLetter;
+           //we make the Letter be equal to aLetter
+           Letter = aLetter;
         }
+
+        //Getter / Setter
+        public string Letter
+        {
+            get { return letter; }
+            set
+            {
+                if (value == "A" || value == "B" || value == "C")
+                {
+                    letter = value;
+                }
+                else
+                {
+                    letter = "No letter";
+                }
+
+            }
+            
+
+
+
+        }
+
+
 
         //Object method
         public bool HasHonors()
